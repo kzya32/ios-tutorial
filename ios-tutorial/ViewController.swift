da99ce7d580321b4aad1304f729fc8093ff28193
KazuyaFujimori <fjky9132@gmail.com>
2019-03-13 15:33:18 +0900

    Initial Commit

---
+++ ios-tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-tutorial
+//
+//  Created by 藤森 一矢 on 2019/03/13.
+//  Copyright © 2019 Kazuya Fujimori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
